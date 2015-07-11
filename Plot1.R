7c884b050920b2c31c78a6c4d5e95a0b99814c9c
hector-ps <psanchezhector@gmail.com>
2015-07-11 14:03:15 +0200

    First commit

---
+++ Plot1.R
@@ -0,0 +1,40 @@
+
+# Getting the names of the columns.
+data.names <- read.table("household_power_consumption.txt", 
+                    header = FALSE,
+                    sep = ";", 
+                    nrows = 1,
+                    stringsAsFactors = FALSE,
+                    na.strings = "?")
+
+# Getting the data from the desired rows
+consumption <- read.table("household_power_consumption.txt", 
+                   header = FALSE,
+                   sep = ";", 
+                   skip = 66637,
+                   nrows = 2880,
+                   stringsAsFactors = FALSE,
+                   na.strings = "?")
+
+# Assigning the names to the data.frame columns
+colnames(consumption) <- data.names
+
+# Editting the "Date" column to be *Date Type*
+consumption$Date <- as.Date(consumption$Date, "%d/%m/%Y")
+#consumption$Time <- as.POSIXct(consumption$Time, "%H:%M:%S")
+
+# Time <- paste(consumption$Date, consumption$Time)
+consumption$Time <- strptime(paste(consumption$Date, consumption$Time), "%Y-%m-%d %H:%M:%S")
+
+png("plot1.png", width=480, height=480)
+
+hist(consumption$Global_active_power, 
+     col = "red", 
+     n = 12,
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)")
+
+dev.off()
+
+
+
